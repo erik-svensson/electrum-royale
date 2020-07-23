26f41b7877b56f5ef0fdc89515ec806f22dde1b7
Mariusz Twarowski <f20f1a85b3@protonmail.com>
2020-07-23 09:38:23 +0200

    Add tx type selection on send tab

--- electrum/gui/qt/three_keys_windows.py
+++ electrum/gui/qt/three_keys_windows.py
@@ -4,10 +4,11 @@ from functools import partial
 from PyQt5.QtCore import Qt, QRect
 from PyQt5.QtGui import QStandardItemModel, QStandardItem
 from PyQt5.QtWidgets import QVBoxLayout, QLabel, QPushButton, QWidget, QHBoxLayout, QLineEdit, \
-    QTreeView, QAbstractItemView, QHeaderView, QStyleOptionButton, QStyle
+    QTreeView, QAbstractItemView, QHeaderView, QStyleOptionButton, QStyle, QGridLayout, QCompleter, QComboBox
 
+from .amountedit import BTCAmountEdit, MyLineEdit, AmountEdit
 from .confirm_tx_dialog import ConfirmTxDialog
-from .util import read_QIcon, WaitingDialog
+from .util import read_QIcon, WaitingDialog, HelpLabel, EnterButton
 from .main_window import ElectrumWindow
 from electrum.i18n import _
 from ... import bitcoin
@@ -57,7 +58,7 @@ class TableItem(QStandardItem):
         self.setTextAlignment(Qt.AlignRight)
 
 
-class ElectrumARWindow(ElectrumWindow):
+class ElectrumMultikeyWalletWindow(ElectrumWindow):
     LABELS = ['Date', 'Confirmation', 'Balance']
 
     def __init__(self, gui_object: 'ElectrumGui', wallet: 'Abstract_Wallet'):
@@ -102,6 +103,7 @@ class ElectrumARWindow(ElectrumWindow):
             def sign_done(success):
                 if success:
                     self.broadcast_or_show(tx, invoice=invoice)
+
             self.sign_tx_with_password(tx, sign_done, password, recovery_keypairs)
         else:
             self.preview_tx_dialog(make_tx, outputs, external_keypairs=external_keypairs, invoice=invoice)
@@ -120,3 +122,120 @@ class ElectrumARWindow(ElectrumWindow):
 
     def show_recovery_tab(self):
         self.tabs.setCurrentIndex(self.tabs.indexOf(self.recovery_tab))
+
+
+class ElectrumARWindow(ElectrumMultikeyWalletWindow):
+
+    def __init__(self, gui_object: 'ElectrumGui', wallet: 'Abstract_Wallet'):
+        super().__init__(gui_object=gui_object, wallet=wallet)
+
+
+class ElectrumAIRWindow(ElectrumMultikeyWalletWindow):
+
+    def __init__(self, gui_object: 'ElectrumGui', wallet: 'Abstract_Wallet'):
+        super().__init__(gui_object=gui_object, wallet=wallet)
+
+    def create_send_tab(self):
+        # A 4-column grid layout.  All the stretch is in the last column.
+        # The exchange rate plugin adds a fiat widget in column 2
+        self.send_grid = grid = QGridLayout()
+        grid.setSpacing(8)
+        grid.setColumnStretch(3, 1)
+
+        from .paytoedit import PayToEdit
+        self.amount_e = BTCAmountEdit(self.get_decimal_point)
+        self.payto_e = PayToEdit(self)
+        msg = _('Recipient of the funds.') + '\n\n' \
+              + _(
+            'You may enter a Bitcoin address, a label from your list of contacts (a list of completions will be proposed), or an alias (email-like address that forwards to a Bitcoin address)')
+        payto_label = HelpLabel(_('Pay to'), msg)
+        grid.addWidget(payto_label, 1, 0)
+        grid.addWidget(self.payto_e, 1, 1, 1, -1)
+
+        completer = QCompleter()
+        completer.setCaseSensitivity(False)
+        self.payto_e.set_completer(completer)
+        completer.setModel(self.completions)
+
+        msg = _('Description of the transaction (not mandatory).') + '\n\n' \
+              + _(
+            'The description is not sent to the recipient of the funds. It is stored in your wallet file, and displayed in the \'History\' tab.')
+        description_label = HelpLabel(_('Description'), msg)
+        grid.addWidget(description_label, 2, 0)
+        self.message_e = MyLineEdit()
+        self.message_e.setMinimumWidth(700)
+        grid.addWidget(self.message_e, 2, 1, 1, -1)
+
+        msg = _('Amount to be sent.') + '\n\n' \
+              + _('The amount will be displayed in red if you do not have enough funds in your wallet.') + ' ' \
+              + _(
+            'Note that if you have frozen some of your addresses, the available funds will be lower than your total balance.') + '\n\n' \
+              + _('Keyboard shortcut: type "!" to send all your coins.')
+        amount_label = HelpLabel(_('Amount'), msg)
+        grid.addWidget(amount_label, 3, 0)
+        grid.addWidget(self.amount_e, 3, 1)
+
+        self.fiat_send_e = AmountEdit(self.fx.get_currency if self.fx else '')
+        if not self.fx or not self.fx.is_enabled():
+            self.fiat_send_e.setVisible(False)
+        grid.addWidget(self.fiat_send_e, 3, 2)
+        self.amount_e.frozen.connect(
+            lambda: self.fiat_send_e.setFrozen(self.amount_e.isReadOnly()))
+
+        self.max_button = EnterButton(_("Max"), self.spend_max)
+        self.max_button.setFixedWidth(100)
+        self.max_button.setCheckable(True)
+        grid.addWidget(self.max_button, 3, 3)
+
+        msg = _('Choose transaction type.') + '\n\n' + \
+              _('Alert - confirmed after 24h, reversible.') + '\n' + \
+              _('Instant - confirmed immediately, non-reversible. Needs an additional signature.')
+        tx_type_label = HelpLabel(_('Transaction type'), msg)
+        self.tx_type_combo = QComboBox()
+        self.tx_type_combo.addItems([_('alert'), _('instant')])
+        self.tx_type_combo.setCurrentIndex(0)
+        grid.addWidget(tx_type_label, 4, 0)
+        grid.addWidget(self.tx_type_combo, 4, 1)
+
+        self.save_button = EnterButton(_("Save"), self.do_save_invoice)
+        self.send_button = EnterButton(_("Pay"), self.do_pay)
+        self.clear_button = EnterButton(_("Clear"), self.do_clear)
+
+        buttons = QHBoxLayout()
+        buttons.addStretch(1)
+        buttons.addWidget(self.clear_button)
+        buttons.addWidget(self.save_button)
+        buttons.addWidget(self.send_button)
+        grid.addLayout(buttons, 6, 1, 1, 4)
+
+        self.amount_e.shortcut.connect(self.spend_max)
+
+        def reset_max(text):
+            self.max_button.setChecked(False)
+            enable = not bool(text) and not self.amount_e.isReadOnly()
+            # self.max_button.setEnabled(enable)
+
+        self.amount_e.textEdited.connect(reset_max)
+        self.fiat_send_e.textEdited.connect(reset_max)
+
+        self.set_onchain(False)
+
+        self.invoices_label = QLabel(_('Outgoing payments'))
+        from .invoice_list import InvoiceList
+        self.invoice_list = InvoiceList(self)
+
+        vbox0 = QVBoxLayout()
+        vbox0.addLayout(grid)
+        hbox = QHBoxLayout()
+        hbox.addLayout(vbox0)
+        hbox.addStretch(1)
+        w = QWidget()
+        vbox = QVBoxLayout(w)
+        vbox.addLayout(hbox)
+        vbox.addStretch(1)
+        vbox.addWidget(self.invoices_label)
+        vbox.addWidget(self.invoice_list)
+        vbox.setStretchFactor(self.invoice_list, 1000)
+        w.searchable_list = self.invoice_list
+        run_hook('create_send_tab', grid)
+        return w
