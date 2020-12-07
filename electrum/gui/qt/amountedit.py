65a37bb56dc9e30bca28808293fcc66d5a68a2a9
Konrad Jawulski <konrad.jawulski@minebest.com>
2020-12-07 11:53:44 +0100

    Added suppress on copy, paste and cut in amount field

--- electrum/gui/qt/amountedit.py
+++ electrum/gui/qt/amountedit.py
@@ -3,7 +3,7 @@
 from decimal import Decimal
 
 from PyQt5.QtCore import pyqtSignal, Qt
-from PyQt5.QtGui import QPalette, QPainter, QFontMetrics
+from PyQt5.QtGui import QPalette, QPainter, QFontMetrics, QKeySequence
 from PyQt5.QtWidgets import (QLineEdit, QStyle, QStyleOptionFrame)
 
 from .util import char_width_in_lineedit
@@ -45,8 +45,6 @@ class AmountEdit(MyLineEdit):
         if text == '!':
             self.shortcut.emit()
             return
-        if ',' in text:
-            text = text[: text.find(',')]
 
         pos = self.cursorPosition()
         chars = '0123456789'
@@ -58,9 +56,8 @@ class AmountEdit(MyLineEdit):
                 s = s.replace('.','')
                 s = s[:p] + '.' + s[p:p+self.max_precision()]
 
-        if len(s) > 0 and s != '0' and '.' not in s:
-            s = s.lstrip('0')
-            s = '0' if not s else s
+        if len(s) > 1 and s[0] == '0' and '.' not in s:
+            s = '0'
         self.setText(s)
         # setText sets Modified to False.  Instead we want to remember
         # if updates were because of user modification.
@@ -86,6 +83,16 @@ class AmountEdit(MyLineEdit):
 
     def setAmount(self, x):
         self.setText("%d"%x)
+
+    def contextMenuEvent(self, event):
+        """Suppress context menu"""
+        pass
+
+    def keyPressEvent(self, event):
+        """Suppress ctrl+c, ctrl+v ctrl+x"""
+        if event.matches(QKeySequence.Paste) or event.matches(QKeySequence.Copy) or event.matches(QKeySequence.Cut):
+            return
+        super().keyPressEvent(event)
 
 
 class BTCAmountEdit(AmountEdit):
